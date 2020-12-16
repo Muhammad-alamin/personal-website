70998d9adada9ebf421720e499ededaa4ac687e9
Muhammad-alamin <alamin.softdevloper@gmail.com>
2020-12-16 19:20:20 +0600

    feature-template mastering done

---
+++ resources/views/admin/layouts/_footer.blade.php
@@ -0,0 +1,5 @@
+<strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+All rights reserved.
+<div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.1.0-pre
+</div>
